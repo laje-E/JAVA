dbcd6196bd28cf16547869c1dabeb0700d448573
laje-E <136092273+laje-E@users.noreply.github.com>
2025-06-05 09:38:01 -0300

    subo la carpeta del TP-1, con los ejercicio 1 y 2

---
+++ TP_1_LPOO/src/ejercicio_2.java
@@ -0,0 +1,29 @@
+
+public class ejercicio_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=50;i<=100;i++) {    //se inicia el ciclo for del 50 al 100 para ver que valores son primos
+			System.out.println("");
+			int no_primo = 0;
+			int j=2;				  //j se va a encargar de explorar todas las posiciones hasta llegar a i-1
+			
+			do {					  //este ciclo do se hace para que se pueda aumentar j hasta llegar a i-1
+				
+				if(i%j == 0) {	      //se verifica el resto, si es 0 el numero NO es primo
+					no_primo++;
+					System.out.println("el divisor de " + i + "es: " + j);
+				}
+				j++;
+			}while (j<i);
+		
+			if (no_primo >= 1) {
+				System.out.println("no es primo");
+			}
+			else if (no_primo == 0) {
+				System.out.println("es primo");
+			}
+		}
+	}
+
+}
