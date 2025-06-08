aaf8c230bdb3b2377765e5b2bc3f064b495731c1
Eluney Laje <eluneylaje@gmail.com>
2025-06-08 18:42:59 -0300

    se agregaron los comentarios a los ejercicios 5 y 6

--- TP_1_LPOO/src/ejercicio_5.java
+++ TP_1_LPOO/src/ejercicio_5.java
@@ -10,13 +10,16 @@ public class ejercicio_5 {
 		do {
 		System.out.println("ingrese el numero del que quiere tener la tabla de multiplicar (debe ser del 1 al 10)");
 		numero = scanner.nextInt();
-		}while(numero < 1 || numero > 10);
+		}while(numero < 1 || numero > 10); //se hace esta verificacion para que el usuario no ingrese un numero erroneo. 
+										   //en caso de que lo ingrese vuelve a mostrar el mensaje para que ingrese un numero.
 		int n = 10;
 		int i = 1;
 		int tabla;
-		while(i <= n) {
-			tabla = numero * i;
-			System.out.println(i + "= | " + tabla + " |");
+		while(i <= n) {					  //se hace un ciclo para que la tabla vaya aumentando en 1. 
+										  //para poder asi mostrar todos los valores del 1 al 10 inclusive
+			
+			tabla = numero * i;			  //los valores de la multiplicacion se van a guardar en una variable separada al numero
+			System.out.println(i + "= | " + tabla + " |");  //los resultados se van imprimiendo durante el ciclo while
 			i++;
 		}
 	}
--- TP_1_LPOO/src/ejercicio_6.java
+++ TP_1_LPOO/src/ejercicio_6.java
@@ -9,14 +9,17 @@ public class ejercicio_6 {
 		String palabra;
 		System.out.println("ingrese una palabra o frase para ser invertida");
 		palabra = scanner.nextLine();
-		int n = palabra.length();
+		int n = palabra.length();						//se guarda el valor de la longitud de la palabra ingresada por el usuario. 
+														//para luego poder trabajar con ella.
 		int i = 0;
 		int j = n;
 		System.out.print("La palabra invertida es: ");
-		while (j > 0) {
-			System.out.print(palabra.charAt(j));
+		while (i <= n) {
+			System.out.print(palabra.charAt(j));	//lo que se va a hacer es: se va a imprimir cada letra desde la ultima hasta la primera
+													//esto va a producir que la palabra este dada vuelta ya que las ultimas palabras son las primeras en ser imprimidas
+													//esto se va a producir durante el ciclo while y con print solo para no generar un salto de linea y que asi esten todas las letras juntas
 			i++;
-			j--;
+			j--;									//se le reduce en 1 el valor a j para que llegue a 0 y asi se impriman todas las ubicaciones de la palabra
 		}
 	}
 
