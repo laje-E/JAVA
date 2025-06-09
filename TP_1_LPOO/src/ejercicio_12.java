59b6cb14ab17e8cae0123e9052e1b05d93feedf7
Eluney Laje <eluneylaje@gmail.com>
2025-06-09 19:35:49 -0300

    se agrego el ejercicio 12 CORREGIR

---
+++ TP_1_LPOO/src/ejercicio_12.java
@@ -0,0 +1,18 @@
+package tp1;
+
+import java.util.Scanner;
+public class ejercicio_12 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new scanner(System.in);
+		
+		int numero;
+		System.out.println("ingrese un numero con 2 o mas cifras");
+		
+		numero = scanner.nextInt();
+		
+		int cifras;
+
+	}
+
+}
