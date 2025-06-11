d890427df132d28e788bdabeb587099b3c8eacd9
Eluney Laje <eluneylaje@gmail.com>
2025-06-11 13:00:24 -0300

    se agrego el ejercicio 20 completo

--- TP_1_LPOO/src/ejercicio_20.java
+++ TP_1_LPOO/src/ejercicio_20.java
@@ -9,17 +9,23 @@ public class ejercicio_20 {
 		Random random = new Random();
 		
 		int n;
-		System.out.println("ingrese la cantidad de caracteres que desea que tenga su contrasenia");
+		System.out.println("ingrese la cantidad de caracteres que desea que tenga su contrasenia");		//le solicito al cliente que indique la longitud de la contraseña
 		n = scanner.nextInt();
-		String contraseña = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		int longitud = contraseña.length();
+		String contraseña = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";			//todas las opciones que tiene para escribir la contraseña
+		int longitud = contraseña.length();																//la longitud de todos los caracteres que puede tener la contraseña
 		int i = 0;
+		char caracter_contraseña;
 		
+		System.out.println("la contraseña es: ");
 		while (i<n) {
-			contraseña.charAt(i) = random.nextLine(longitud);
+			caracter_contraseña = contraseña.charAt(random.nextInt(longitud));							//se guarda cada caracter en una variable para luego ser imprimida
+			
+			System.out.print(caracter_contraseña);														//se imprime cada caracter
 			
 			i++;
 		}
+		
+		
 		scanner.close();
 
 	}
