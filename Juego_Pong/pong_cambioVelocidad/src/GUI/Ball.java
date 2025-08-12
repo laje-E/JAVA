4d04e53502647813c1dde43461462afaef7e0e4d
laje-E <eluneylaje@gmail.com>
2025-08-11 21:56:46 -0300

    se agrego la version beta de la pelota con velocidad

--- Juego_Pong/pong_cambioVelocidad/src/GUI/Ball.java
+++ Juego_Pong/pong_cambioVelocidad/src/GUI/Ball.java
@@ -1,136 +1,96 @@
 package GUI;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class Ball extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	int dx = 2; 
-	int dy = 2;
-	int cambio_velocidadX = 1;
-	int cambio_velocidadY = 1;
-	private Puntaje puntaje;
+	double dx = 2; 
+	double dy = 2;
+	double cambioVelocidad = 1.1;
 
 	/**
 	 * Create the panel.
 	 */
-	public Ball(int posX, int posY, int ancho, int altura, Puntaje puntaje) {
-        this.puntaje = puntaje;
-        setBounds(posX, posY, ancho, altura);
-    }
-	public void mover() {
-		/*
-		int dx = 2; //delta x, o sea la cantidad de pixeles que se va a mover la pelota, por lo tanto es la velocidad
-		int dy = 2; //delta y, velocidad
-		*/
-		int posY = getY();
-		int posX = getX();
-		posX += dx;
-		posY += dy;
-		setLocation(posX, posY);
+	public Ball(int posX, int posY, int ancho, int altura) {
+		setBounds(posX, posY, ancho, altura);
 	}
-	public void rebotar(Player player1, Player player2) {
-		
+	public void mover() {
 		/*
 		int dx = 2; //delta x, osea la cantidad de pixeles que se va a mover la pelota, por lo tanto es la velocidad
 		int dy = 2; //delta y, velocidad
 		*/
-		int posY = getY();
-		int posX = getX();
+		double posY = getY();
+		double posX = getX();
+		posX += dx;
+		posY += dy;
+		setLocation(posX, posY);
+	}
+	private void setLocation(double posX, double posY) {
+		// TODO Auto-generated method stub
 		
-// Si pasa por el lado izquierdo, es punto del 2
-		if (posX <= 0) {
-            puntaje.addScore(2);
-            resetBall(300, 150, 1); 
-            return;
-        }
-		
-// Si pasa por el lado derecho, es punto del 1
-        if (posX >= 600 - getWidth()) {
-            puntaje.addScore(1);
-            resetBall(300, 150, -1); 
-            return;
-        }
-
+	}
+	public void rebotar(Player player1, Player player2) {
+		/*
+		int dx = 2; //delta x, osea la cantidad de pixeles que se va a mover la pelota, por lo tanto es la velocidad
+		int dy = 2; //delta y, velocidad
+		*/
+		double posY = getY();
+		double posX = getX();
 		if (posX <= 0 || posX >= 600 - 20) {						// 600 es el valor absoluto del ancho de la pestaña y 20 el ancho de la pelota
 																	// la primer condición es si se toca la pared izquierda, 
 																	// y la segunda es si se toca la derecha, que se hace el ancho de la cancha menos el ancho de la pelota
 																	// se hace la resta ya que la posicion de "x" se toma desde arriba a la izquierda del objeto "pelota"
-			if (dx > 0) {
-				dx = (-1) * (dx + cambio_velocidadX);			//se invierte la dirección de la pelota
-			}
-			if (dy > 0){
-				dy = (dy + cambio_velocidadY);
-			}
-			if (dy <= 0) {
-				dy = (dy - cambio_velocidadY);
-			}
-			if (dx <= 0) {
-				dx = (-1) * (dx - cambio_velocidadX);			//se invierte la dirección de la pelota
-			}
+			dx = -dx;  												// se invierte el movimiento de la pelota una vez rebota en la pared derecha o izquierda
+			
+			dx *= cambioVelocidad;
+			
+			dy *= cambioVelocidad;
 		}
 		
 		if (posY <= 0 || posY >= 310 - 20) {						// 310 es el valor absoluto del alto de la pestaña y 20 el alto de la pelota
 																	// la primer condición es si se toca la parte de arriba de la pantalla, 
 																	// y la segunda es si se toca la parte de abajo de la pantalla menos la altura de la pelota
 																	// se hace la resta ya que la posicion de "y" se toma desde arriba a la izquierda del objeto "pelota"
-			if (dy > 0) {
-				dy = (-1) * (dy + cambio_velocidadX);			//se invierte la dirección de la pelota
-			}
-			if (dx > 0){
-				dx = (dx + cambio_velocidadY);
-			}
-			if (dx <= 0) {
-				dx = (dx - cambio_velocidadY);
-			}
-			if (dy <= 0) {
-				dy = (-1) * (dy - cambio_velocidadX);			//se invierte la dirección de la pelota
-			}
+			dy = -dy;  												// se invierte el movimiento de la pelota una vez rebota 	
+			
+			dx *= cambioVelocidad;
+			
+			dy *= cambioVelocidad;
 		}
 		if (posX <= player1.getX() + player1.getWidth() &&			// se compara la posición X de la pelota con la posición X del jugador 1 mas el ancho que tiene la paleta
 			posX + getWidth() >= player1.getX() && 					// se compara la posición X de la pelota, mas su ancho, con la posición X de la paleta 1
 			posY <= player1.getY() + player1.getHeight() &&			// se compara la posición Y de la pelota con la posición Y del jugador 1 mas la altura que tiene la paleta
 			posY + getHeight() >= player1.getY()) {					// se compara la posición Y de la pelota, mas su altura, con la posición Y de la paleta 1 
-			if (dx > 0) {
-				dx = (-1) * (dx + cambio_velocidadX);			//se invierte la dirección de la pelota
-			}
-			if (dy > 0){
-				dy = (dy + cambio_velocidadY);
-			}
-			if (dy <= 0) {
-				dy = (dy - cambio_velocidadY);
-			}
-			if (dx <= 0) {
-				dx = (-1) * (dx - cambio_velocidadX);			//se invierte la dirección de la pelota
-			}	
+				dx = -dx;					//se invierte la dirección de la pelota
+				
+				dx *= cambioVelocidad;
+				
+				dy *= cambioVelocidad;
 		}
 		
 		if (posX <= player2.getX() + player2.getWidth() &&			// se compara la posición X de la pelota con la posición X del jugador 2 mas el ancho que tiene la paleta
 			posX + getWidth() >= player2.getX() && 					// se compara la posición X de la pelota, mas su ancho, con la posición X de la paleta 2
 			posY <= player2.getY() + player2.getHeight() &&			// se compara la posición Y de la pelota con la posición Y del jugador 2 mas la altura que tiene la paleta
 			posY + getHeight() >= player2.getY()) {					// se compara la posición Y de la pelota, mas su altura, con la posición Y de la paleta 2
-			if (dx > 0) {
-				dx = (-1) * (dx + cambio_velocidadX);			//se invierte la dirección de la pelota
+				dx = -dx;				//se invierte la dirección de la pelota
+				
+				dx *= cambioVelocidad;
+				
+				dy *= cambioVelocidad;
+				
 			}
-			if (dy > 0){
-				dy = (dy + cambio_velocidadY);
-			}
-			if (dy <= 0) {
-				dy = (dy - cambio_velocidadY);
-			}
-			if (dx <= 0) {
-				dx = (-1) * (dx - cambio_velocidadX);			//se invierte la dirección de la pelota
-			}
-		}
 		
 		posX += dx;
 		posY += dy;
+		setLocation(posX, posY);
 	}
-	private void resetBall(int x, int y, int dirX) {
-        setLocation(x, y);
-        dx = 2 * dirX; // direccion hacia quien perdio
-        dy = 2;
-    }
+
+
+	
 	/*
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -141,3 +101,5 @@ public class Ball extends JPanel {
 	*/
 	
 }
+
+
